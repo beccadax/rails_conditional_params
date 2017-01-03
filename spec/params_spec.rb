20c61d52a16f03fb77fdabbd7238c7fd669c83a4
Brent Royal-Gordon <brent@architechies.com>
2017-01-02 20:25:30 -0800

    Avoid Rails 5 deprecation

--- spec/params_spec.rb
+++ spec/params_spec.rb
@@ -7,14 +7,14 @@ describe ActionController::Parameters do
   end
   
   it "should work the same without conditionals" do
-    expect(@params.permit(:allowed)).to eq("allowed" => "allowed")
+    expect(@params.permit(:allowed).to_h).to eq("allowed" => "allowed")
   end
   
   it "should include conditionals when conditionals are true" do
-    expect(@params.permit(:allowed, maybe: true)).to eq("allowed" => "allowed", "maybe" => "maybe")
+    expect(@params.permit(:allowed, maybe: true).to_h).to eq("allowed" => "allowed", "maybe" => "maybe")
   end
   
   it "should exclude conditionals when conditionals are false" do
-    expect(@params.permit(:allowed, maybe: false)).to eq("allowed" => "allowed")
+    expect(@params.permit(:allowed, maybe: false).to_h).to eq("allowed" => "allowed")
   end
 end
