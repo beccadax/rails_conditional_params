d0495594cce80335b4021ae1aad94a949178bccf
Brent Royal-Gordon <brent@architechies.com>
2017-01-13 17:49:59 -0800

    Ensure original param list isn’t affected
    I got a little nervous about this and decided to add a test, just in case.

--- spec/params_spec.rb
+++ spec/params_spec.rb
@@ -17,4 +17,15 @@ describe ActionController::Parameters do
   it "should exclude conditionals when conditionals are false" do
     expect(@params.permit(:allowed, maybe: false).to_h).to eq("allowed" => "allowed")
   end
+  
+  it "should not carry over allowed-ness between separate calls" do
+    def filtered(maybe: false)
+      @params.permit(:allowed, maybe: maybe).to_h
+    end
+    
+    expect(filtered(maybe: false).to_h).to eq("allowed" => "allowed")
+    expect(filtered(maybe: true).to_h).to eq("allowed" => "allowed", "maybe" => "maybe")
+    expect(filtered(maybe: false).to_h).to eq("allowed" => "allowed")
+    expect(filtered(maybe: true).to_h).to eq("allowed" => "allowed", "maybe" => "maybe")
+  end
 end
